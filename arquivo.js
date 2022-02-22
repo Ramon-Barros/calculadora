0ccb31717d8350be3a0b89ff71cae71c1e2337d1
Ramon Barros <ramonfilipe@gmail.com>
2022-02-21 21:14:02 -0300

    ampliando mais a calculadora

--- arquivo.js
+++ arquivo.js
@@ -13,10 +13,15 @@ function back(){
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
 function calcular(){
+    
     var resultado = document.getElementById('resultado').innerHTML;
     if(resultado){
         document.getElementById('resultado').innerHTML = eval(resultado);
     } else {
         document.getElementById('resultado').innerHTML = "ERRO!!"
     }
+}
+function porcento(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado /100;
 }
