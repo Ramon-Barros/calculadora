db9cf2aaca83da2fc3bd160d17897c51ba058e97
Ramon Barros <ramonfilipe@gmail.com>
2022-02-21 21:31:17 -0300

    colocando funções raiz quadrada e porcento

--- arquivo.js
+++ arquivo.js
@@ -1,3 +1,4 @@
+//funções de calculo 
 function insert(num){
 
     var numero = document.getElementById('resultado').innerHTML;
@@ -8,10 +9,12 @@ function insert(num){
 function clean(){
     document.getElementById('resultado').innerHTML = "";
 }
+
 function back(){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 }
+
 function calcular(){
     
     var resultado = document.getElementById('resultado').innerHTML;
@@ -21,10 +24,12 @@ function calcular(){
         document.getElementById('resultado').innerHTML = "ERRO!!"
     }
 }
+
 function porcento(){
     var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado / 100;
 }
+
  function raiz(){
      var resultado = document.getElementById('resultado').innerHTML;
      document.getElementById('resultado').innerHTML = Math.sqrt(resultado);
